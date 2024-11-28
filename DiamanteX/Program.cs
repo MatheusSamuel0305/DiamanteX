624e3889dac2c0888a69d06f6b8beab72ae8728d
MatheusSamuel0305 <Matheussamuel05@gmail.com>
2024-11-28 15:11:31 -0300

    first commit

---
+++ DiamanteX/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Programa {
+    static void Main() {
+        Console.WriteLine("Digite um número ímpar:");
+        int n = int.Parse(Console.ReadLine());
+        if (n % 2 == 0) {
+            Console.WriteLine("Número inválido.");
+            return;
+        }
+
+        int meio = n / 2;
+
+        for (int i = 0; i < n; i++) {
+            int espacos = Math.Abs(meio - i);
+            int xs = n - 2 * espacos;
+            Console.WriteLine(new string(' ', espacos) + new string('x', xs));
+        }
+    }
+}
